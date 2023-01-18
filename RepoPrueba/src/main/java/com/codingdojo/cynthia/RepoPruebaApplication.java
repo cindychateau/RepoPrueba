ce926150535dfd6c3645381ddaff7a872b1aeef6
Cynthia Castillo <ca.castillo@gmail.com>
2023-01-18 15:20:54 -0600

    Commit inicial

---
+++ RepoPrueba/src/main/java/com/codingdojo/cynthia/RepoPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.cynthia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoPruebaApplication.class, args);
+	}
+
+}
